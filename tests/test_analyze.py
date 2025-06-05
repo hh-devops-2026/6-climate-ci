fc6c446d35d812686be7a872d3f8bbcf576e5b24
juhahinkula <hinkula@gmail.com>
2025-06-05 12:37:36 +0300

    intial commit

--- tests/test_analyze.py
+++ tests/test_analyze.py
@@ -2,7 +2,12 @@ import sys
 import os
 import tempfile
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '../src')
+    )
+)
 from src.analyze import analyze_and_plot
 
 
